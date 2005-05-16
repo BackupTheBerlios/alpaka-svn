48be749cf9c9392c6cb2429da5d6a32f32a7eefd
canpaniel <canpaniel@aeddac06-e5df-0310-a822-85481781c49f>
2005-05-16 23:26:21 +0000

    Examples moved to examples directory

---
+++ trunk/Alpaka/examples/lib/SampleApp/Example1.pm
@@ -0,0 +1,47 @@
+package SampleApp::Example1;
+use strict;
+  
+use base 'Alpaka::Component';
+
+sub example1 {
+    my ($self, $request, $response, $session, $app) = @_;
+    
+    my $count = $session->get('count') || 0;
+    $response->write("<h1>Example No.1 ($count)</h1>");
+    $count++;
+    $session->set('count' => $count);
+    
+}
+
+
+sub index {
+    my ($self, $request, $response, $session, $app) = @_;
+    
+    $self->forward('example1');
+        
+}
+
+
+sub redirect {
+    my ($self, $request, $response, $session, $app) = @_;
+    
+    $response->redirect( $app->base_path . "/example1.do");
+    
+}
+
+sub header { 
+	my ($self, $request, $response, $session, $app) = @_;
+	
+	$response->write("<table bgcolor=\"#aaaaee\"><tr><td>");
+	
+
+}
+
+sub footer { 
+	my ($self, $request, $response, $session, $app) = @_;
+	
+	$response->write("</td></tr></table>");
+}
+
+
+1;
